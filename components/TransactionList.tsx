f291f1873fa47a38d8dced3f42bceec211101da0
iSalah-YT <82966610+iSalah-YT@users.noreply.github.com>
2024-06-29 13:09:04 +0100

    project completed

---
+++ components/TransactionList.tsx
@@ -0,0 +1,25 @@
+import getTransaction from '@/app/actions/getTransaction';
+import TransactionItem from './TransactionItem';
+import { Transaction } from '@/types/Transaction';
+
+const TransactionList = async () => {
+  const { transactions, error } = await getTransaction();
+
+  if (error) {
+    return <p className="error">{error}</p>;
+  }
+
+  return (
+    <>
+      <h3>History</h3>
+      <ul className="list">
+        {transactions &&
+          transactions.map((transaction: Transaction) => (
+            <TransactionItem key={transaction.id} transaction={transaction} />
+          ))}
+      </ul>
+    </>
+  );
+};
+
+export default TransactionList;
